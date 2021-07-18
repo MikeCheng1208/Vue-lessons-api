efcc5d98a1b08ea5b8ac5e86c53a77b65fe7e0a4
Mike <a3804430@hotmail.com>
2021-07-19 07:19:05 +0800

    fix if

--- server.js
+++ server.js
@@ -124,14 +124,11 @@ app.post("/testToken", (req, res) => {
   const token = req.headers.authorization;
   let statue_code = 200;
   const content = {};
-  if (token !== "636b6030-3ee3-11eb-b378-0242ac130002") {
-    statue_code = 403;
-    content["error_message"] = "無效的 token";
-  } else if (!token) {
-    statue_code = 403;
-    content["error_message"] = "無效的 token";
-  } else {
+  if (token === "636b6030-3ee3-11eb-b378-0242ac130002") {
     content["success"] = "ok";
+  } else {
+    statue_code = 403;
+    content["error_message"] = "無效的 token";
   }
   res.status(statue_code).send(content);
 });
